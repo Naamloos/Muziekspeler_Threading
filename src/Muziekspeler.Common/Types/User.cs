149153a0f0cadc18847564a39d8ea333e5ff3e21
Zanril95 <jeroen-wind@live.nl>
2021-03-31 09:47:47 +0200

    added User methods and changed DisplayName type

--- src/Muziekspeler.Common/Types/User.cs
+++ src/Muziekspeler.Common/Types/User.cs
@@ -6,10 +6,31 @@ namespace Muziekspeler.Common.Types
 {
     public class User
     {
-        public int Id;
+        public int Id { get; set; }
 
-        public int DisplayName;
+        public string DisplayName { get; set; } = "Defaulter";
 
         public string Status = "Jammin'";
+
+        public User() {
+            // Id = userID komt uit server, onbekend hoe ATM;
+        }
+
+        public void setDisplayName(string name) {
+            DisplayName = name;
+        }
+
+        public string getDisplayName() {
+            return DisplayName;
+        }
+
+        public void setStatus(string status) {
+            Status = status;
+        }
+
+        public string getStatus() {
+            return Status;
+        }
+        // Client stuurt displayName en status naar server, server verstuurt user object terug
     }
 }
